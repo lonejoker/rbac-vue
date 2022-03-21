67f979f6e573abef7c839fc87bffc03d9f2741f6
xiaobai <1582294124@qq.com>
2022-03-22 00:40:36 +0800

    尝试添加用户功能，可自定义角色名，选定角色后可看到对应的权限

--- src/views/system/user/User.vue
+++ src/views/system/user/User.vue
@@ -8,7 +8,7 @@
 				<el-button class="mrb" @click="reset">重置</el-button>
 			</div>
 			<div style="margin-bottom: 10px">
-				<el-button type="primary"><i class="el-icon-circle-plus"></i>添加</el-button>
+				<el-button type="primary" @click="addUser"><i class="el-icon-circle-plus"></i>添加</el-button>
 				<el-button type="danger" slot="reference"><i class="el-icon-delete-solid"></i>批量删除</el-button>
 			</div>
 			<el-table :data="userData" stripe style="width: 100%" ref="multipleTable">
@@ -35,7 +35,7 @@
 						<el-tag v-if="scope.row.userType == 0">管理员</el-tag>
 						<el-tag v-else type="success">普通用户</el-tag>
 					</template>
-        </el-table-column>
+				</el-table-column>
 				<el-table-column prop="roleName" label="用户角色" width="130" align="center"></el-table-column>
 				<el-table-column prop="status" label="用户状态" width="140" align="center">
 					<template slot-scope="scope">
@@ -53,17 +53,52 @@
 					</template>
 				</el-table-column>
 			</el-table>
-      <div class="pagina">
-        <el-pagination
-            @size-change="handleSizeChange"
-            @current-change="handleCurrentChange"
-            :current-page="this.pageNum"
-            :page-sizes="[5, 10, 15, 20]"
-            :page-size="this.pageSize"
-            layout="total, sizes, prev, pager, next, jumper"
-            :total="total">
-        </el-pagination>
-      </div>
+			<!-- 分页 -->
+			<div class="pagina">
+				<el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="this.pageNum" :page-sizes="[5, 10, 15, 20]" :page-size="this.pageSize"
+					layout="total, sizes, prev, pager, next, jumper" :total="total">
+				</el-pagination>
+			</div>
+			<!-- 添加用户 -->
+			<el-dialog title="添加用户" :visible.sync="dialogFormVisibleAdd" width="30%">
+				<el-form label-width="80px" size="small">
+					<el-form-item label="用户名">
+						<el-input v-model.trim="addForm.userName" autocomplete="off"></el-input>
+					</el-form-item>
+					<el-form-item label="密码">
+						<el-input v-model.trim="addForm.password" autocomplete="off"></el-input>
+					</el-form-item>
+					<el-form-item label="角色">
+						<el-select clearable v-model="addForm.roleName" @change="changeRoles" filterable allow-create default-first-option placeholder="请选择角色" style="width: 100%">
+							<el-option v-for="item in roles" :key="item.name" :label="item.name" :value="item.flag"></el-option>
+						</el-select>
+					</el-form-item>
+					<el-form-item label="邮箱">
+						<el-input v-model.trim="addForm.email" autocomplete="off"></el-input>
+					</el-form-item>
+					<el-form-item label="手机号码">
+						<el-input v-model.trim="addForm.phone" autocomplete="off"></el-input>
+					</el-form-item>
+					<el-form-item label="性别">
+						<el-input v-model.trim="addForm.sex" autocomplete="off"></el-input>
+					</el-form-item>
+				</el-form>
+				<div slot="footer" class="dialog-footer">
+					<el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+					<el-button type="primary" @click="saveUser">确 定</el-button>
+				</div>
+				<div v-show="isChange">
+					<el-card class="box-card">
+						<div slot="header" class="clearfix">
+							<span>卡片名称</span>
+							<el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+						</div>
+						<div v-for="o in 4" :key="o" class="text item">
+							{{'列表内容 ' + o }}
+						</div>
+					</el-card>
+				</div>
+			</el-dialog>
 		</el-card>
 	</div>
 </template>
@@ -80,7 +115,20 @@ export default {
 			pageSize: 5,
 			total: 0,
 			nickName: '',
-			userData: []
+			userData: [],
+			dialogFormVisibleAdd: false,
+			addForm: {},
+			roles: [{
+				name: 'HTML',
+				flag: 'HTML'
+			}, {
+				name: 'CSS',
+				flag: 'CSS'
+			}, {
+				name: 'JavaScript',
+				flag: 'JavaScript'
+			}],
+			isChange: false
 		};
 	},
 	watch: {},
@@ -89,17 +137,17 @@ export default {
 		// 当每页数据发生改变时触发
 		handleSizeChange (val) {
 			this.pageSize = val
-      this.getUserInfo()
+			this.getUserInfo()
 		},
 		// 当当前页码发生改变时触发
 		handleCurrentChange (val) {
-      this.pageNum = val
-      this.getUserInfo()
+			this.pageNum = val
+			this.getUserInfo()
 		},
 		// 重置输入框
 		reset () {
-      this.nickName = ''
-      this.getUserInfo()
+			this.nickName = ''
+			this.getUserInfo()
 		},
 		// 回车搜索
 		searchEnterFun (e) {
@@ -124,12 +172,29 @@ export default {
 				}
 			}).then(res => {
 				if (res.data.code == 200) {
-					console.log(res)
+					// console.log(res)
 					this.userData = res.data.data.records
 					this.total = res.data.data.total
 				}
 			})
 		},
+		// 提交数据
+		saveUser () {
+			console.log(121212)
+			console.log(this.addForm);
+		},
+		// 添加用户弹窗
+		addUser () {
+			this.dialogFormVisibleAdd = true
+			this.addForm = {}
+		},
+		changeRoles (e) {
+			console.log(222222);
+			this.isChange = true
+      if(e == ''){
+        this.isChange = false
+      }
+		}
 	},
 	created () {
 		this.getUserInfo()
@@ -138,4 +203,24 @@ export default {
 };
 </script>
 <style lang="less" scoped>
+.text {
+	font-size: 14px;
+}
+
+.item {
+	margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+	display: table;
+	content: "";
+}
+.clearfix:after {
+	clear: both;
+}
+
+.box-card {
+	width: 480px;
+}
 </style>
