3a2194039e453db2e1c65d28cdee623541cb1dfa
xiaobai <1582294124@qq.com>
2022-03-28 12:55:13 +0800

    优化文件管理，组件化

--- src/views/files/Files.vue
+++ src/views/files/Files.vue
@@ -1,53 +1,49 @@
 <template>
 	<div class="wrapper">
-    <el-tabs  v-model="activeName" type="border-card" @tab-click="handleClick" >
-      <el-tab-pane v-for="item in typeList" :key="item.name" :label="item.label" :name="item.name">
-        <JPG v-if="activeName == 'jpg'"></JPG>
-        <PNG v-if="activeName == 'png'"></PNG>
-        <Mp4 v-if="activeName == 'mp4'"></Mp4>
-      </el-tab-pane>
-    </el-tabs>
-  </div>
+		<el-tabs v-model="activeName" type="border-card" @tab-click="handleClick">
+			<el-tab-pane v-for="item in typeList" :key="item.name" :label="item.label" :name="item.name">
+				<Demo v-if="activeName == item.name" :types="activeName" />
+			</el-tab-pane>
+		</el-tabs>
+	</div>
 </template>
 
 <script>
-import JPG from '@/views/files/Jpg.vue'
-import PNG from '@/views/files/Png.vue'
-import Mp4 from '@/views/files/Mp4.vue'
+import Demo from '@/views/files/demo.vue'
 export default {
 	name: 'Files',
 	components: {
-    JPG,PNG,Mp4
-  },
+		Demo
+	},
 	props: {},
 	data () {
 		return {
-      activeName: 'png',
-      typeList:[],
+			activeName: 'jpg',
+			typeList: [],
 		};
 	},
 	watch: {},
 	computed: {},
 	methods: {
-    handleClick(tab, event) {
-        this.activeName = tab.name
-    },
-    getTypeList(){
-      this.http.get("/sysFiles/getTypeList").then((result) => {
-          this.typeList = result.data.data
-      }).catch((err) => {
-        this.$message.error("获取列表失败",err);
-      });
-    },
+		handleClick (tab, event) {
+			this.activeName = tab.name
+		},
+		getTypeList () {
+			this.http.get("/sysFiles/getTypeList").then((result) => {
+				this.typeList = result.data.data
+			}).catch((err) => {
+				this.$message.error("获取列表失败", err);
+			});
+		},
 	},
 	created () {
-    this.getTypeList()
-  },
+		this.getTypeList()
+	},
 	mounted () { }
 };
 </script>
 <style lang="less" scoped>
-/deep/ .el-tabs--border-card>.el-tabs__content{
-  padding: 20px;
+/deep/ .el-tabs--border-card > .el-tabs__content {
+	padding: 20px;
 }
 </style>
